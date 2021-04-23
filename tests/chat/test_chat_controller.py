1f3337bf61ba0e957d56cda87e94fc4b7b9ec6f5
michal <michal.wilk0@yahoo.com>
2021-04-23 13:12:23 +0200

    added basic chat_controller test

--- tests/chat/test_chat_controller.py
+++ tests/chat/test_chat_controller.py
@@ -1,20 +1,41 @@
-# from app.chat.chat_controller import ChatController
-# from app.user_state import UserState
+from app.chat.chat_controller import ChatController
+from app.user_state import UserState 
+import re
+import asyncio
+import pytest
+from unittest.mock import AsyncMock
 
-def setup_chat_controller():
-    # state = UserState("majki", "passw")
-    # state.user_id = "aaaaaaaaaaaa"
-    # chat = ChatController(state)
-    # return chat, state
-    return 1,2
-
-
-def test_sending_message():
-    chat, state = setup_chat_controller()
-    # chat.send("hello this is a test message")
-    import app
-    # pp = 
-    assert 1 == 1
+def test_get_timestamp():
+    expr = re.search(
+        "[0-9]{2}:[0-9]{2}:[0-9]{2}",
+        ChatController.get_timestamp()
+    )
+    assert expr is not None
     
+
+@pytest.mark.asyncio
+async def test_websocket_worker(mocker):
+    state = UserState("alice", "password")
+    chat_controller = ChatController(state, "bob")
+
+    chat_controller.messageQueue = asyncio.Queue()
+
+    async def get_message_side_effect():
+        return "Message1234"
+
+    mocker.patch(
+        'app.api.websocket_controller.WebSocketController.get_message',
+        side_effect = get_message_side_effect
+    )
+
+    task = asyncio.create_task(chat_controller.websocket_worker())
+    result = await asyncio.wait_for(
+        chat_controller.messageQueue.get(), 2
+    )
+    task.cancel()
+    chat_controller.messageQueue.task_done()
+    
+    assert result.sender == 'bob'
+    assert result.body == 'Message1234'
 
 
