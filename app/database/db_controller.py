1e3d3e27aa9b1a04a9a48acb83c6f9737a970632
michal <michal.wilk0@yahoo.com>
2021-04-23 19:20:15 +0200

    created tests for the database controller

--- app/database/db_controller.py
+++ app/database/db_controller.py
@@ -1,17 +1,43 @@
 from typing import List
 from app.user_state import UserState
+import sqlite3
 
 class DatabaseController:
     # TODO: IMPLEMENT THIS CLASS!!!
-    def __init__(self, user_state:UserState):
-        self.user_state = user_state
+    def __init__(self, DB_PATH='db_data/user.db'):
+        self.connection = sqlite3.connect(DB_PATH)
 
-    def update_user_id(self):
-        self.user_state.user_id = 'dsajkndsadnaksnk'
+        if not self.file_correct():
+            self._reinstall()
 
-    def get_user_contacts(self) -> List[str]:
-        return [
-            "Adam",
-            "John",
-            "Mark"
-        ]
+    def file_correct(self):
+        ...
+
+    def _reinstall(self):
+        ...
+
+    # User crud operations
+
+    def create_user(self, new_user_state:UserState):
+        ...
+
+    def user_exists(self, user_state:UserState):
+        ...
+
+    def delete_user(self, user_state:UserState):
+        ...
+
+    # Contacts crud operations
+
+    def add_contact(self, state:UserState, contactLogin:str):
+        ...
+
+    def delete_contact(self, user_state:UserState, contactLogin:str):
+        ...
+
+    def contact_exists(self, user_state:UserState, contactLogin:str):
+        ...
+
+    def get_user_contacts(self, state:UserState) -> List[str]:
+        return
+    
