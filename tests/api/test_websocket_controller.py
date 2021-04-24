6c1ddf698e3edd113c40a1421de6300a3fc8ed07
michal <michal.wilk0@yahoo.com>
2021-04-24 20:25:10 +0200

    added empty test for websocket_controller

---
+++ tests/api/test_websocket_controller.py
@@ -0,0 +1,18 @@
+from app.api.websocket_controller import WebSocketController
+import pytest
+
+async def test_establish_connection():
+    ...
+
+async def test_send_raw_message():
+    ...
+
+async def test_get_message():
+    ...
+
+async def test_should_throw_when_no_connection():
+    ...
+
+async def test_should_throw_when_closing_closed_connection():
+    ...
+
