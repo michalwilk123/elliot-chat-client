b9fdd9b100db8aa767d39b3cb861ddd67919a846
michal <michal.wilk0@yahoo.com>
2021-04-29 12:29:26 +0200

    udpdated schema

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -2,12 +2,17 @@
 CREATE TABLE USERS(
     login VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_key TEXT,
+    signed_pre_key TEXT,
+    send_ratchet TEXT,
+    recv_ratchet TEXT
 );
 
 CREATE TABLE CONTACTS(
     owner VARCHAR(255) NOT NULL,
     login VARCHAR(255) NOT NULL,
+    shared_x3dh_key TEXT,
     FOREIGN KEY(login) REFERENCES USERS(login),
     PRIMARY KEY (owner, login)
 );
