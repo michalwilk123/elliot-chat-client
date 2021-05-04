664a95f37e7a7fb462bb599ed695f10614084829
michal <michal.wilk0@yahoo.com>
2021-05-04 23:14:12 +0200

    added empty tests

--- tests/chat/test_chat_db_integration.py
+++ tests/chat/test_chat_db_integration.py
@@ -1,2 +1,39 @@
 
 
+
+def test_add_new_contact(mocker):
+    """
+    Test adding new contact to the database 
+    (without full handshake). But now with mocked 
+    server
+    """
+    assert False
+
+
+def test_continue_contact_add(mocker):
+    """
+    Scenario: 
+        user A adds user B and quits the app
+
+    He wants for the next app start to be 
+    already connected with user B
+
+    So we need 2 app controllers
+    """
+    assert False
+
+def test_approve_contact_add(mocker):
+    """
+    Scenario:
+        User A added user B as a new contact.
+        At this time B had the app closed.
+        He now starts his app normally.
+
+        He now wants to have user A
+        in his contact list after startup.
+
+        In the background we do all the initial
+        ratchet rotations and handshake 
+        shenanigans
+    """
+    assert False
---
+++ tests/test_app_controller.py
@@ -0,0 +1,8 @@
+
+
+
+def test_check_xavier(mocker):
+    """
+    Check if xavier is alive
+    """
+    assert False
