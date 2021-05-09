eee80fe0e324021bcfa442ac24e928a99139cb70
michal <michal.wilk0@yahoo.com>
2021-05-10 01:02:43 +0200

    deleted unnessasary code

--- app/chat/message.py
+++
@@ -1,7 +0,0 @@
-class Message:
-    __slots__ = "sender", "time_stamp", "body"
-
-    def __init__(self, sender: str, time_stamp: str, body: str):
-        self.sender: str = sender
-        self.time_stamp: str = time_stamp
-        self.body: str = body
