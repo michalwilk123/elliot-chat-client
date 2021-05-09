4a0032204efad1509b49ce21c78f2b0c68b4ac22
michal <michal.wilk0@yahoo.com>
2021-05-10 01:14:13 +0200

    added connection to the backend

---
+++ app/api/api_controller.py
@@ -0,0 +1,246 @@
+import binascii
+from app.chat.crypto.crypto_utils import (
+    create_b64_from_private_key,
+    create_b64_from_public_key,
+    create_public_key_from_b64,
+    create_shared_key_X3DH_establisher,
+)
+from app.routes import ApiRoutes
+from app.config import (
+    FETCH_DELAY_PERIOD,
+    MAX_ONE_TIME_KEYS,
+    PREFFERED_ENCODING,
+    SERVER_URL,
+)
+from typing import Optional, Tuple, List
+from aiohttp.client import ClientSession
+from app.user.user_state import UserState
+import random
+from app.database.db_controller import DatabaseController
+import asyncio
+
+
+class ApiControllerException(Exception):
+    ...
+
+
+class ApiController:
+    def __init__(
+        self,
+        user_state: UserState,
+        db_controller: DatabaseController,
+        /,
+        url: str = SERVER_URL,
+        init_session: bool = True,
+        register_user: bool = False,
+    ) -> None:
+        self._client_session: Optional[ClientSession] = (
+            ClientSession() if init_session else None
+        )
+        self.user_state = user_state
+        self._url = url
+        self.db_controller = db_controller
+        self._contact: Optional[str] = None
+        self.register_user = register_user
+
+    @property
+    def contact(self) -> str:
+        if self._contact is not None:
+            return self._contact
+        else:
+            raise ApiControllerException("Contact property not set!!!")
+
+    @contact.setter
+    def contact(self, contact: str) -> None:
+        self._contact = contact
+
+    @property
+    def client_session(self) -> ClientSession:
+        if self._client_session is None:
+            raise ApiControllerException("client session not set!")
+        return self._client_session
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    async def end(self):
+
+        await self.client_session.close()
+
+    async def server_task(self):
+        if self.register_user:
+            await self.create_user()
+
+        while True:
+            await self.resolve_new_contacts(),
+            await asyncio.sleep(FETCH_DELAY_PERIOD)
+
+    async def create_user(self):
+        user_model = {
+            "login": self.user_state.login,
+            "public_id_key": create_b64_from_public_key(
+                self.user_state.id_key.public_key()
+            ),
+            "public_signed_pre_key": create_b64_from_public_key(
+                self.user_state.signed_pre_key.public_key()
+            ),
+            "signature": self.user_state.signature,
+            "number_of_otk": MAX_ONE_TIME_KEYS,
+            "one_time_keys": None,
+        }
+        async with self.client_session.post(
+            ApiRoutes.USER_OPER, json=user_model
+        ) as resp:
+            if resp.status == 200:
+                print(f"Successfuly added new user: {self.user_state.login}")
+            else:
+                print(f"Could not add user {self.user_state.login}")
+                print(await resp.text())
+                raise ApiControllerException("Could not add user!!")
+
+        otk_keys = self.db_controller.get_user_otk(self.user_state)
+        otk_initial = {
+            "login": self.user_state.login,
+            "signature": self.user_state.signature,
+            "num_of_keys": MAX_ONE_TIME_KEYS,
+            "one_time_public_keys": otk_keys,
+        }
+        async with self.client_session.post(
+            self._url + ApiRoutes.ONE_TIME_KEY, json=otk_initial
+        ) as resp:
+            if resp.status == 200:
+                print(
+                    "Finish one time key setup. "
+                    f"Got response: {await resp.json()}"
+                )
+            else:
+                print("Could not setup otk keys")
+                print(await resp.text())
+
+    def approve_new_contact(self, invite: dict) -> str:
+        curr_otk, new_otk = self.db_controller.replace_one_time_key(
+            self.user_state, invite["otk_index"]
+        )
+        new_otk_b64 = create_b64_from_private_key(new_otk).decode(
+            PREFFERED_ENCODING
+        )
+
+        if (
+            self.user_state.id_key is None
+            or self.user_state.signed_pre_key is None
+        ):
+            raise ApiControllerException("All user keys must be present!")
+
+        shared_key = create_shared_key_X3DH_establisher(
+            self.user_state.id_key,
+            self.user_state.signed_pre_key,
+            curr_otk,
+            create_public_key_from_b64(invite["public_id_key"]),
+            create_public_key_from_b64(invite["public_ephemeral_key"]),
+        )
+
+        self.db_controller.add_contact(
+            self.user_state,
+            invite["my_login"],
+            invite["public_id_key"],
+            binascii.b2a_base64(shared_key).decode(PREFFERED_ENCODING),
+            contact_ephemeral_key=invite["public_ephemeral_key"],
+            my_otk_key=create_b64_from_private_key(curr_otk).decode(PREFFERED_ENCODING)
+        )
+        return new_otk_b64
+
+    async def send_friend_request(self, invite: dict):
+
+        async with self.client_session.post(self._url + ApiRoutes.CONTACT_OPER, json=invite) as resp:
+            result = await resp.json()
+
+
+
+    async def resolve_new_contacts(self):
+        params = {
+            "login": self.user_state.login,
+            "signature": self.user_state.signature.decode(PREFFERED_ENCODING),
+        }
+        async with self.client_session.get(
+            self._url + ApiRoutes.CONTACT_OPER, params=params
+        ) as resp:
+            if resp.status == 200:
+                inv_list = list((await resp.json())["invites"])
+                if inv_list:
+                    print(f"Got {len(inv_list)} new invites!")
+            else:
+                print("Could not retrieve new pending invites!")
+                print(await resp.text())
+                return
+        for invite in inv_list:
+            new_otk = self.approve_new_contact(invite)
+            otk_model = {
+                "login": self.user_state.login,
+                "signature": self.user_state.signature.decode(
+                    PREFFERED_ENCODING
+                ),
+                "one_time_public_key": new_otk,
+                "otk_index": invite["otk_index"],
+            }
+            async with self.client_session.put(
+                self._url + ApiRoutes.ONE_TIME_KEY, json=otk_model
+            ) as resp:
+                if resp.status == 200:
+                    print(await resp.json())
+                else:
+                    print(f"Could not set otk key: {await resp.text()}")
+
+    async def check_contact(self, contact: str) -> bool:
+        async with self.client_session.get(
+            self._url + ApiRoutes.USER_OPER, params={"login": contact}
+        ) as resp:
+            return resp.status == 200
+
+    async def init_conversation(self, message_queue: asyncio.Queue) -> None:
+        data = {
+            "login": self.user_state.login,
+            "signature": self.user_state.signature,
+        }
+        async with self.client_session.get(
+            self._url + ApiRoutes.CHAT_OPER, json=data
+        ) as resp:
+            for m in (await resp.json())["pending_messages"]:
+                # TODO : might be problematic.
+                # Dont know the sequence of messages
+                await message_queue.put(m)
+
+    async def get_contact_info(self, contact: str):
+        async with self.client_session.get(
+            self._url + ApiRoutes.USER_OPER, params={"login": contact}
+        ) as resp:
+            return await resp.json()
+
+    async def get_random_one_time_key(
+        self,
+    ) -> Tuple[Optional[str], int]:
+        async with self.client_session.get(
+            self._url + ApiRoutes.ONE_TIME_KEY_LIST,
+            params={"contact": self.contact},
+        ) as resp:
+            if resp.status == 200:
+                avail_list: List[int] = (await resp.json())[
+                    "available_indexes"
+                ]
+            else:
+                print(await resp.text())
+                return None, resp.status
+
+        rand_idx = random.choice(avail_list)
+
+        async with self.client_session.get(
+            self._url + ApiRoutes.ONE_TIME_KEY,
+            params={"login": self.contact, "index": rand_idx},
+        ) as resp:
+            if resp.status == 200:
+                key: str = (await resp.json())["one_time_key"]
+            else:
+                print(await resp.text())
+                return None, resp.status
+
+        return key, rand_idx
--- app/app_controller.py
+++ app/app_controller.py
@@ -1,67 +1,208 @@
-from app.api.response_controller import ResponseController
-from app.chat.crypto.establisher import Establisher
-from .user_state import UserState
+import binascii
+from typing import Optional
+
+from app.chat.crypto.crypto_utils import (
+    create_b64_from_private_key,
+    create_b64_from_public_key,
+    create_public_key_from_b64,
+    generate_DH,
+    create_shared_key_X3DH_guest,
+)
+from .api.api_controller import ApiController
+from .user.user_state import UserState
 from .cli import utilities, chat
-from .config import MainMenuOptions, SERVER_URL
+from .config import (
+    DEFAULT_DB_PATH,
+    MainMenuOptions,
+    PREFFERED_ENCODING,
+    SERVER_URL,
+)
 from .database.db_controller import DatabaseController
 from .chat.chat_controller import ChatController
-from app.api.api_util_operations import get_pending_responses
+import asyncio
+import urllib.request
+from colorama import init, deinit
+import urllib.error
+from .routes import ApiRoutes
+
+
+class AppControllerException(Exception):
+    ...
+
+
+class ContactNotFoundException(Exception):
+    ...
+
+
+class NotOneTimeKeysLeft(Exception):
+    ...
 
 
 class AppController:
-    __slots__ = "__user_state", "__db_controller", "__chat_controller"
-
-    def __init__(self):
+    def __init__(self, db_path: str = DEFAULT_DB_PATH):
+        self._url = SERVER_URL
         self.resolve_startup()
         self.db_controller = DatabaseController()
-        utilities.startup()
-        login, password = utilities.get_credentials()
+        self._db_path = db_path
 
-        self.user_state = UserState(login, password)
-        self.response_controller = ResponseController(self.user_state, SERVER_URL)
-        self.establisher = Establisher(self.user_state, self.response_controller, load_from_db=True)
-
-    def choose_reciever(self) -> str:
+    def choose_reciever(self) -> Optional[str]:
         """choosing contact to have chat with
         Returns:
             str: chosen contact login
         """
         contacts = self.db_controller.get_user_contacts(self.user_state)
-        return contacts[chat.choose_contact(contacts)]
+        contacts.append("Back")
+        chosen = chat.choose_contact(contacts)
+        if chosen == len(contacts) - 1:
+            return None
+        return contacts[chosen]
 
     def resolve_startup(self):
-        ...
+        try:
+            resp = urllib.request.urlopen(self._url + ApiRoutes.ALIVE)
+            resp.close()
+            pass
+        except urllib.error.URLError:
+            print(f"Cannot connect to the server URL: {self._url}")
 
-    def add_new_user_contact(self):
-        ...
-
-    def get_blocking_server_message(self):
-        ...
+        init(autoreset=True)
 
     def start(self):
+        utilities.startup()
+        login = utilities.get_credentials()
+
+        self.user_state = UserState(login)
+        user_exist = self.db_controller.user_exists(self.user_state)
+
+        if not user_exist:
+            self.db_controller.create_user(self.user_state)
+        else:
+            id_key, sg_key = self.db_controller.get_user_keys(self.user_state)
+            self.user_state.id_key = id_key
+            self.user_state.signed_pre_key = sg_key
+
+        # resolve_user_state(self.user_state, self._db_path)
+
+        self.api_controller = ApiController(
+            self.user_state, self.db_controller, register_user=not user_exist
+        )
+
+        loop = asyncio.get_event_loop()
+        serv_task = loop.create_task(self.api_controller.server_task())
+        loop.run_until_complete(self.main_menu())
+        serv_task.cancel()
+
+        """
+        tutaj powinien sie tworzyc event loop
+        i powinien od razu podpiac taska z establisherem
+
+        Oprócz tego powinien byc rowniez podpiety
+        task z samą aplikacja
+
+        !!!!! USUN WEBSOCKETS !!!!!
+        """
+
+    async def main_menu(self):
         while True:
-            # option = utilities.get_menu_option()
-            option = MainMenuOptions.MESSAGE
+            option = utilities.get_menu_option()
 
             if option == MainMenuOptions.MESSAGE:
                 reciever = self.choose_reciever()
+                if reciever is None:
+                    continue
                 self.__chat_controller = ChatController(
-                    self.user_state, reciever, self.establisher
+                    self.api_controller, reciever
                 )
 
                 self.__chat_controller.start()
                 del self.__chat_controller
             elif option == MainMenuOptions.ADD_FRIEND:
-                pass
+                contact_name = await utilities.get_contact_name()
+                await self.add_contact(contact_name)
             elif option == MainMenuOptions.CHANGE_CREDENTIALS:
-                pass
+                print("not implemented")
             elif option == MainMenuOptions.REMOVE_ACCOUNT:
-                pass
+                print("not implemented")
             elif option == MainMenuOptions.WAITROOM:
-                pass
+                await self.init_waitroom()
             elif option == MainMenuOptions.EXIT:
                 print("bye")
                 break
             else:
                 raise RuntimeError("This option currently is not implemented")
         print("cleanup")
+        await self.api_controller.client_session.close()
+        deinit()
+
+    async def init_waitroom(self):
+        ...
+
+    async def add_contact(self, contact: str):
+        if not await self.api_controller.check_contact(contact):
+            raise ContactNotFoundException()
+        
+        self.api_controller.contact = contact
+
+        contact_info, (one_time_key, otk_idx) = await asyncio.gather(
+            self.api_controller.get_contact_info(contact),
+            self.api_controller.get_random_one_time_key(),
+        )
+
+        if not contact_info["success"]:
+            print("Given contact does not exist!!")
+            return
+
+        if one_time_key is None:
+            print("User has already used up his one time keys.")
+            return
+
+        contact_info = contact_info["user_data"]
+        ephemeral_key = generate_DH()
+
+        if contact_info["login"] != contact:
+            AppControllerException(
+                "Somehow you got wrong user! "
+                f"You got {contact_info['login']} "
+                f"rather than {contact}"
+            )
+
+        await self.api_controller.send_friend_request(
+            {
+                "my_login": self.user_state.login,
+                "signature" : self.user_state.signature,
+                "contact_login": contact,
+                "public_id_key": create_b64_from_public_key(
+                    self.user_state.id_key.public_key()
+                ).decode(PREFFERED_ENCODING),
+                "public_ephemeral_key": create_b64_from_public_key(
+                    ephemeral_key.public_key()
+                ).decode(PREFFERED_ENCODING),
+                "otk_index": otk_idx,
+            }
+        )
+
+        if self.user_state.id_key is None:
+            raise AppControllerException("somehow my id key dissapeard")
+
+        shared_key = create_shared_key_X3DH_guest(
+            self.user_state.id_key,
+            ephemeral_key,
+            create_public_key_from_b64(contact_info["public_id_key"].encode(PREFFERED_ENCODING)),
+            create_public_key_from_b64(contact_info["public_signed_pre_key"].encode(PREFFERED_ENCODING)),
+            create_public_key_from_b64(
+                one_time_key.encode(PREFFERED_ENCODING)
+            ),
+        )
+
+        self.db_controller.add_contact(
+            self.user_state,
+            contact,
+            contact_info["public_id_key"],
+            binascii.b2a_base64(shared_key).decode(PREFFERED_ENCODING),
+            contact_signed_pre_key=contact_info["public_signed_pre_key"],
+            my_ephemeral_key=create_b64_from_private_key(ephemeral_key).decode(
+                PREFFERED_ENCODING
+            ),
+            contact_otk_key=one_time_key
+        )
+        print(f"You have send {contact} an invite")
